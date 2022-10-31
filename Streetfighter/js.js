bbb5b56dc6862b84d6cc2b344ee04cbed3f6a37c
mavu1707 <markus.yung.vullum@gmail.com>
2022-10-31 13:57:52 +0100

    Street_fighter_ide

---
+++ Streetfighter/js.js
@@ -0,0 +1,48 @@
+let player1 = [
+/*
+  HP
+  Damage
+  Navn
+  startposisjon
+*/
+]
+let player2 = [
+/*
+  HP
+  Damage
+  Navn
+  startposisjon
+*/
+]
+
+/*
+  Tegne opp canvas
+  Sette opp bakgrunn
+*/
+
+function movement(){ //Player 1 function movement_player_1
+/*
+  Arrowkeys
+  (ENTER for hopping)
+  ha en if test som sjekker om player1 treffer player2
+*/
+}
+
+function movement(){ //Player 2 function movement_player_2
+  /*
+    WASD
+    (space for hopping)
+    ha en if test som sjekker om player1 treffer player2
+  */
+  }
+
+  function drawElements(){
+    /*
+    clearrect
+    clearcanvas
+
+    drawPlayer1
+    drawPlayer2
+    */
+    drawElements()
+  }
